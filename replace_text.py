d2adec68a56efdc8be0a4291510f438d7feb56c0
4nybody <119219336+4nybody@users.noreply.github.com>
2023-11-10 09:41:50 +0800

    Delete replace_text.py

--- replace_text.py
+++
@@ -1,9 +0,0 @@
-import re
-import fileinput
-
-def replace_in_file(file_path: object, old_string: object, new_string: object) -> object:
-    with fileinput.FileInput(file_path, inplace=True, backup='.bak') as file:
-        for line in file:
-            print(re.sub(old_string, new_string, line), end='')
-
-replace_in_file("out_window.py", "on_clockButton_clicked", "on_clockInButton_clicked")
